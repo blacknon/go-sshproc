7fbfc2629ad7e6c4bdba44fe5fc8a6aa2e7e913c
blacknon <blacknon@orebibou.com>
2024-07-07 10:34:24 +0900

    update.

--- _example/example_process_list.go
+++ _example/example_process_list.go
@@ -1,8 +1,6 @@
-// Copyright (c) 2021 Blacknon. All rights reserved.
+// Copyright(c) 2024 Blacknon. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
-//
-// cpuinfoを実現するためのテストコード.
 
 package main
 
